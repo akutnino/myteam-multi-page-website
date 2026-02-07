3495e1400e82fa9045f133e8f7cea3afd84b0c11
Nino Akut <akutnino@gmail.com>
2026-02-08 02:37:30 +0800

    updated App component - applied react router and lazy loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,40 @@
+import { lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import Layout from './common/Layout.tsx';
+
+const Home = lazy(() => import('./pages/Home.tsx'));
+const About = lazy(() => import('./pages/About.tsx'));
+const Contact = lazy(() => import('./pages/Contact.tsx'));
+
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={<Layout />}
+				>
+					<Route
+						index={true}
+						element={<Home />}
+					/>
+					<Route
+						path='about'
+						element={<About />}
+					/>
+					<Route
+						path='contact'
+						element={<Contact />}
+					/>
+					<Route
+						path='*'
+						element={<Home />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
