155427c1df702a819679db928ac178c84406bbba
Nino Akut <akutnino@gmail.com>
2026-02-08 02:13:24 +0800

    updated About component - added about page components

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,5 +1,15 @@
+import AboutHeader from '../contents/about/AboutHeader.tsx';
+import Clients from '../contents/about/Clients.tsx';
+import Directors from '../contents/about/Directors.tsx';
+
 function About() {
-	return;
+	return (
+		<>
+			<AboutHeader />
+			<Directors />
+			<Clients />
+		</>
+	);
 }
 
 export default About;
