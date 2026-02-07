858a9cef3c2b568e2c0127df6e53e7cc9cc4d061
Nino Akut <akutnino@gmail.com>
2026-02-08 01:08:09 +0800

    updated Home component - added home page components

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,15 @@
+import Features from '../contents/home/Features.tsx';
+import HomeHeader from '../contents/home/HomeHeader.tsx';
+import Testimonials from '../contents/home/Testimonials.tsx';
+
 function Home() {
-	return;
+	return (
+		<>
+			<HomeHeader />
+			<Features />
+			<Testimonials />
+		</>
+	);
 }
 
 export default Home;
