f5b3d4c30a98532d8ebb3da84758bdb3a962c85e
Nino Akut <akutnino@gmail.com>
2026-02-08 02:35:48 +0800

    updated Layout component - added boundaries and components

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,5 +1,34 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Outlet, useLocation, type Location } from 'react-router';
+
+import OutletWrapper from './OutletWrapper.tsx';
+import LoadingSpinner from './LoadingSpinner.tsx';
+import Home from '../pages/Home.tsx';
+import Footer from './Footer.tsx';
+import CallToAction from './CallToAction.tsx';
+
 function Layout() {
-	return;
+	const location: Location = useLocation();
+	const isPathnameContact: boolean = location.pathname === 'contact';
+
+	return (
+		<ErrorBoundary
+			key={location.key}
+			fallback={<Home />}
+		>
+			<Suspense
+				key={location.key}
+				fallback={<LoadingSpinner />}
+			>
+				<OutletWrapper>
+					<Outlet />
+				</OutletWrapper>
+				{!isPathnameContact && <CallToAction />}
+				<Footer />
+			</Suspense>
+		</ErrorBoundary>
+	);
 }
 
 export default Layout;
