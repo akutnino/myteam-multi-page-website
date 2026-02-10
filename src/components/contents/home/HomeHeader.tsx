5d41522b9a75755871d08d59117a497e7f8623b9
Nino Akut <akutnino@gmail.com>
2026-02-11 03:19:29 +0800

    updated HomeHeader component - added all elements and classNames

--- src/components/contents/home/HomeHeader.tsx
+++ src/components/contents/home/HomeHeader.tsx
@@ -1,5 +1,43 @@
+import Navigation from '../../common/Navigation.tsx';
+import bg_pattern_home_1 from '/src/assets/bg-pattern-home-1.svg';
+import bg_pattern_home_2 from '/src/assets/bg-pattern-home-2.svg';
+
 function HomeHeader() {
-	return <header></header>;
+	return (
+		<header className='homeHeader'>
+			<Navigation />
+
+			<section className='homeHeader__section'>
+				<h1 className='homeHeader__header'>
+					Find the best <span className='homeHeader__span'>talent</span>
+				</h1>
+
+				<div className='homeHeader__div'>
+					<div className='homeHeader__rectangle' />
+
+					<p className='homeHeader__paragraph'>
+						Finding the right people and building high performing teams can be hard. Most
+						companies aren’t tapping into the abundance of global talent. We’re about to
+						change that.
+					</p>
+				</div>
+			</section>
+
+			<img
+				className='homeHeader__background--first'
+				src={bg_pattern_home_1}
+				alt='bg-pattern-home-1.svg'
+				fetchPriority='high'
+			/>
+
+			<img
+				className='homeHeader__background--second'
+				src={bg_pattern_home_2}
+				alt='bg-pattern-home-2.svg'
+				fetchPriority='high'
+			/>
+		</header>
+	);
 }
 
 export default HomeHeader;
