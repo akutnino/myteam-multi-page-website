5d25e6f55c43f7df9967b31ce1f2132359b72686
Nino Akut <akutnino@gmail.com>
2026-02-11 03:19:07 +0800

    updated Navigation component - added all elements and classNames

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,5 +1,46 @@
+import { Link } from 'react-router';
+import logo from '/src/assets/logo.svg';
+
 function Navigation() {
-	return <nav></nav>;
+	return (
+		<nav className='navigation'>
+			<div className='navigation__div'>
+				<img
+					className='navigation__logo'
+					src={logo}
+					alt='myteam logo.svg'
+					fetchPriority='high'
+				/>
+
+				<ul className='navigation__list'>
+					<li className='navigation__item'>
+						<Link
+							className='navigation__link'
+							to={'/'}
+						>
+							home
+						</Link>
+					</li>
+
+					<li className='navigation__item'>
+						<Link
+							className='navigation__link'
+							to={'/about'}
+						>
+							about
+						</Link>
+					</li>
+				</ul>
+			</div>
+
+			<Link
+				className='navigation__contact'
+				to={'/contact'}
+			>
+				contact us
+			</Link>
+		</nav>
+	);
 }
 
 export default Navigation;
