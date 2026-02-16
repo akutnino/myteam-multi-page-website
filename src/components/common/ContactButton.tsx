5ec99ff53afbf37424863712fadc6026239bb5f2
Nino Akut <akutnino@gmail.com>
2026-02-17 02:33:24 +0800

    updated ContactButton component - added props and render logic

--- src/components/common/ContactButton.tsx
+++ src/components/common/ContactButton.tsx
@@ -1,12 +1,42 @@
+import { useState, type CSSProperties, type ReactNode } from 'react';
 import { Link } from 'react-router';
 
-function ContactButton() {
+function ContactButton({
+	className,
+	children,
+	hoverStyles,
+}: {
+	className: string;
+	hoverStyles: {
+		backgroundColor: '--color-Neutral-0' | '--color-Teal-900';
+		color: '--color-Neutral-0' | '--color-Teal-950';
+	};
+	children: ReactNode;
+}) {
+	const [isHover, setIsHover] = useState<boolean>(false);
+
+	const handleOnMouseEnter = () => {
+		setIsHover((currentState) => !currentState);
+	};
+
+	const handleOnMouseLeave = () => {
+		setIsHover((currentState) => !currentState);
+	};
+
+	const styles: CSSProperties = {
+		backgroundColor: `var(${hoverStyles.backgroundColor})`,
+		color: `var(${hoverStyles.color})`,
+	};
+
 	return (
 		<Link
-			className={`contactButton`}
+			className={className}
+			style={isHover ? styles : undefined}
+			onMouseEnter={handleOnMouseEnter}
+			onMouseLeave={handleOnMouseLeave}
 			to={'/contact'}
 		>
-			contact us
+			{children}
 		</Link>
 	);
 }
