5b62ec24a525ad4c06e41889228ac0a32a21730c
Nino Akut <akutnino@gmail.com>
2026-02-18 01:43:26 +0800

    updated CallToAction component - added all elements and classNames

--- src/components/common/CallToAction.tsx
+++ src/components/common/CallToAction.tsx
@@ -1,5 +1,32 @@
+import ContactButton from './ContactButton.tsx';
+
+import bg_pattern_home_6_about_5 from '/src/assets/bg-pattern-home-6-about-5.svg';
+
 function CallToAction() {
-	return <section></section>;
+	return (
+		<section className='callToAction'>
+			<div className='callToAction__div'>
+				<h1 className='callToAction__h1'>Ready to get started?</h1>
+
+				<ContactButton
+					className='callToAction__contact'
+					hoverStyles={{
+						backgroundColor: '--color-Teal-900',
+						color: '--color-Neutral-0',
+					}}
+				>
+					contact us
+				</ContactButton>
+			</div>
+
+			<img
+				className='callToAction__logo'
+				src={bg_pattern_home_6_about_5}
+				alt='bg-pattern-home-6-about-5.svg'
+				fetchPriority='high'
+			/>
+		</section>
+	);
 }
 
 export default CallToAction;
