25cd866cf5b2de997ca5e4ebef86ae4593d853eb
Nino Akut <akutnino@gmail.com>
2026-02-24 00:35:04 +0800

    updated DirectorsListItem component - added div wrapper element

--- src/components/contents/about/ui/DirectorsListItem.tsx
+++ src/components/contents/about/ui/DirectorsListItem.tsx
@@ -15,6 +15,7 @@ function DirectorsListItem({
 }) {
 	const [isToggled, setIsToggled] = useState<boolean>(false);
 	const isButtonToggled: string = isToggled ? 'directorsListItem__btn--active' : '';
+	const isButtonActive: string = isButtonToggled ? 'directorsListItem__div--active' : '';
 
 	const handleToggle = () => {
 		setIsToggled((currentState) => !currentState);
@@ -22,52 +23,54 @@ function DirectorsListItem({
 
 	return (
 		<li className='directorsListItem'>
-			{!isToggled && (
-				<>
-					<img
-						className='directorsListItem__img--directorImage'
-						src={directorImageSrc}
-						alt='director_image'
-						role='presentation'
-						fetchPriority='high'
-					/>
+			<div className={`directorsListItem__div ${isButtonActive}`}>
+				{!isToggled && (
+					<>
+						<img
+							className='directorsListItem__img--directorImage'
+							src={directorImageSrc}
+							alt='director_image'
+							role='presentation'
+							fetchPriority='high'
+						/>
 
-					<h3 className='directorsListItem__h3--directorName'>{directorName}</h3>
-					<p className='directorsListItem__p--directorPosition'>{directorPosition}</p>
-				</>
-			)}
+						<h3 className='directorsListItem__h3--directorName'>{directorName}</h3>
+						<p className='directorsListItem__p--directorPosition'>{directorPosition}</p>
+					</>
+				)}
 
-			{isToggled && (
-				<>
-					<h3 className='directorsListItem__h3--directorName'>{directorName}</h3>
-					<p className='directorsListItem__p--statement'>
-						“Empowered teams create truly amazing products. Set the north star and let
-						them follow it.”
-					</p>
+				{isToggled && (
+					<>
+						<h3 className='directorsListItem__h3--directorName'>{directorName}</h3>
+						<p className='directorsListItem__p--statement'>
+							“Empowered teams create truly amazing products. Set the north star and let
+							them follow it.”
+						</p>
 
-					<ul className='directorsListItem__ul--iconList'>
-						<li className='directorsListItem__li--icon'>
-							<img
-								className='directorsListItem__img--icon'
-								src={twitter_icon}
-								alt='twitter_icon'
-								role='presentation'
-								fetchPriority='high'
-							/>
-						</li>
+						<ul className='directorsListItem__ul--iconList'>
+							<li className='directorsListItem__li--icon'>
+								<img
+									className='directorsListItem__img--icon'
+									src={twitter_icon}
+									alt='twitter_icon'
+									role='presentation'
+									fetchPriority='high'
+								/>
+							</li>
 
-						<li className='directorsListItem__li--icon'>
-							<img
-								className='directorsListItem__img--icon'
-								src={linkedin_icon}
-								alt='linkedin_icon'
-								role='presentation'
-								fetchPriority='high'
-							/>
-						</li>
-					</ul>
-				</>
-			)}
+							<li className='directorsListItem__li--icon'>
+								<img
+									className='directorsListItem__img--icon'
+									src={linkedin_icon}
+									alt='linkedin_icon'
+									role='presentation'
+									fetchPriority='high'
+								/>
+							</li>
+						</ul>
+					</>
+				)}
+			</div>
 
 			<button
 				className={`directorsListItem__btn--toggle ${isButtonToggled}`}
