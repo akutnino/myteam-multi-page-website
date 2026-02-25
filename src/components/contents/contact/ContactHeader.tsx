06c873039181363bdec06fc72a389e2035980d1a
Nino Akut <akutnino@gmail.com>
2026-02-26 00:14:46 +0800

    updated ContactHeader component - abstracted ContactForm component

--- src/components/contents/contact/ContactHeader.tsx
+++ src/components/contents/contact/ContactHeader.tsx
@@ -1,8 +1,6 @@
-import { useState, type ChangeEvent, type SubmitEvent } from 'react';
-
 import BackgroundImage from '../../common/BackgroundImage.tsx';
 import Navigation from '../../common/Navigation.tsx';
-import FormInput from './ui/FormInput.tsx';
+import ContactForm from './ui/ContactForm.tsx';
 
 import icon_person from '/src/assets/icon-person.svg';
 import icon_cog from '/src/assets/icon-cog.svg';
@@ -11,50 +9,6 @@ import bg_pattern_about_2_contact_1 from '/src/assets/bg-pattern-about-2-contact
 import bg_pattern_contact_2 from '/src/assets/bg-pattern-contact-2.svg';
 
 function ContactHeader() {
-	const [name, setName] = useState<string>('');
-	const [email, setEmail] = useState<string>('');
-	const [company, setCompany] = useState<string>('');
-	const [title, setTitle] = useState<string>('');
-	const [message, setMessage] = useState<string>('');
-
-	const handleNameInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setName(event.target.value);
-	};
-
-	const handleEmailInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setEmail(event.target.value);
-	};
-
-	const handleCompanyInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setCompany(event.target.value);
-	};
-
-	const handleTitleInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setTitle(event.target.value);
-	};
-
-	const handleMessageInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setMessage(event.target.value);
-	};
-
-	const handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
-		event.preventDefault();
-
-		console.log({
-			clientName: name,
-			clientEmail: email,
-			companyName: company,
-			subjectTitle: title,
-			messageBody: message,
-		});
-
-		setName('');
-		setEmail('');
-		setCompany('');
-		setTitle('');
-		setMessage('');
-	};
-
 	return (
 		<header className='contactHeader'>
 			<Navigation />
@@ -72,7 +26,7 @@ function ContactHeader() {
 								className='contactHeader__img'
 								src={icon_person}
 							/>
-							<p>The quality of our talent network</p>
+							<p className='contactHeader__p'>The quality of our talent network</p>
 						</li>
 
 						<li className='contactHeader__li'>
@@ -80,7 +34,7 @@ function ContactHeader() {
 								className='contactHeader__img'
 								src={icon_cog}
 							/>
-							<p>Usage & implementation of our software</p>
+							<p className='contactHeader__p'>Usage & implementation of our software</p>
 						</li>
 
 						<li className='contactHeader__li'>
@@ -88,54 +42,13 @@ function ContactHeader() {
 								className='contactHeader__img'
 								src={icon_chart}
 							/>
-							<p>How we help drive innovation</p>
+							<p className='contactHeader__p'>How we help drive innovation</p>
 						</li>
 					</ul>
 				</section>
 
-				<section className='contactHeader__section--form'>
-					<form
-						className='contactHeader__form'
-						onSubmit={handleSubmit}
-					>
-						<FormInput
-							id='name'
-							placeholder='Name'
-							onChange={handleNameInput}
-							value={name}
-						/>
-						<FormInput
-							type='email'
-							id='email'
-							placeholder='Email Address'
-							onChange={handleEmailInput}
-							value={email}
-						/>
-						<FormInput
-							id='company'
-							placeholder='Company Name'
-							onChange={handleCompanyInput}
-							value={company}
-						/>
-						<FormInput
-							id='title'
-							placeholder='Title'
-							onChange={handleTitleInput}
-							value={title}
-						/>
-						<FormInput
-							id='message'
-							placeholder='Message'
-							onChange={handleMessageInput}
-							value={message}
-						/>
-						<button
-							className='contactHeader__button--submit'
-							type='submit'
-						>
-							submit
-						</button>
-					</form>
+				<section className='contactHeader__section'>
+					<ContactForm />
 				</section>
 			</div>
 
