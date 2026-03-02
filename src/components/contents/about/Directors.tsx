57364dec8b1910d3af6834e7e48e0d54a08a9612
Nino Akut <akutnino@gmail.com>
2026-03-03 00:05:56 +0800

    updated ui folder for components - renamed folder name to contents

--- src/components/contents/about/Directors.tsx
+++ src/components/contents/about/Directors.tsx
@@ -1,4 +1,4 @@
-import DirectorsListItem from './contents/DirectorsListItem.tsx';
+import DirectorsListItem from './elements/DirectorsListItem.tsx';
 import BackgroundImage from '../../common/BackgroundImage.tsx';
 
 import avatar_nikita from '/src/assets/avatar-nikita.jpg';
--- src/components/contents/contact/ContactHeader.tsx
+++ src/components/contents/contact/ContactHeader.tsx
@@ -1,6 +1,6 @@
 import BackgroundImage from '../../common/BackgroundImage.tsx';
 import Navigation from '../../common/Navigation.tsx';
-import ContactForm from './contents/ContactForm.tsx';
+import ContactForm from './elements/ContactForm.tsx';
 
 import icon_person from '/src/assets/icon-person.svg';
 import icon_cog from '/src/assets/icon-cog.svg';
