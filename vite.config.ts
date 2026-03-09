3fe6791bd8c5eaea8a83fb3068b6d1eb8d3d8787
Nino Akut <akutnino@gmail.com>
2026-03-10 00:25:13 +0800

    updated vite.config.ts file - added project base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	return {
+		base: '/myteam-multi-page-website',
 		plugins: [
 			react({
 				babel: {
